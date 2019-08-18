d93cecbea5f150b345ab72226bd27b3939fec909
Floaddy <mateus-pevidor@hotmail.com>
2019-08-18 00:42:00 -0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+require("dotenv/config");
+
+const client = new Discord.Client();
+
+client.login(process.env.BOT_TOKEN);
+
+client.on("message", message => {
+  if (message.content.startsWith("!>roll")) {
+    let messageContent = message.content.split(" ");
+    let roll;
+    if (!isNaN(messageContent[1]))
+      roll = Math.floor(Math.random() * parseInt(messageContent[1])) + 1;
+    else roll = Math.floor(Math.random() * 10) + 1;
+    message.channel.send(`${message.author.username} rolled ${roll}`);
+  }
+});
